6f088ff964a17c8e79ec719b4d372b6322dcbf16
Efim Plotnikov <figishe@gmail.com>
2026-01-05 15:10:38 +0100

    reduce cpu bottleneck, preprocess batch on gpu

--- model/dataset.py
+++ model/dataset.py
@@ -7,10 +7,8 @@ from torch.utils.data import Dataset
 
 class SuperResDataset(Dataset):
 
-    def __init__(self, root, crop_size=192, downscale_denoise=4, downscale=2):
+    def __init__(self, root, crop_size=192):
         self.crop_size = crop_size
-        self.downscale = downscale
-        self.downscale_denoise = downscale_denoise
 
         self.paths = []
         for dirpath, _, filenames in os.walk(root):
@@ -24,34 +22,24 @@ class SuperResDataset(Dataset):
     def __getitem__(self, idx):
         img = Image.open(self.paths[idx])
         w, h = img.size
-        w //= self.downscale_denoise
-        h //= self.downscale_denoise
-
-        img_denoised = img.resize((w, h), Image.LANCZOS)
 
         left = random.randint(0, w - self.crop_size)
-        top = random.randint(0, h - self.crop_size)
-        Y = img_denoised.crop((left, top,
+        top  = random.randint(0, h - self.crop_size)
+
+        Y = img.crop((left, top,
                       left + self.crop_size,
                       top + self.crop_size))
 
-        X = Y.resize(
-            (self.crop_size // self.downscale,
-             self.crop_size // self.downscale),
-            Image.BICUBIC
-        )
+        return self._pil_to_tensor(Y)
 
-        Y_tensor = self._pil_to_tensor(Y)
-        X_tensor = self._pil_to_tensor(X)
-
-        return X_tensor, Y_tensor
 
     def _pil_to_tensor(self, img):
-        x = torch.ByteTensor(torch.ByteStorage.from_buffer(img.tobytes()))
-        x = x.view(img.size[1], img.size[0], 3)  # HxWxC
-        x = x.permute(2, 0, 1).float() / 255.0  # CxHxW
-        x = (x * 2) - 1
-        return x
+        t = torch.from_numpy(
+            np.array(img, dtype=np.uint8)
+        ).permute(2, 0, 1).float() / 255.0
+
+        t = t * 2 - 1   # [-1, 1]
+        return t
     
 
     def tensor_to_pil(self, tensor):
--- model/lit_upscaler.py
+++ model/lit_upscaler.py
@@ -6,25 +6,58 @@ import torch.nn.functional as F
 from torchvision.utils import make_grid
 
 class LitSuperResNet(L.LightningModule):
-    def __init__(self, lr=1e-6, start_channels=64, depth=2):
+    def __init__(self, lr=1e-6, start_channels=64, depth=2, downscale_hires=2, downscale_lowres=2):
         super().__init__()
         self.model = SuperResNet(start_channels=start_channels, depth=depth)
+
+        self.model = torch.compile(
+            self.model,
+            mode="max-autotune",
+            fullgraph=False
+        )
+
         self.lr = lr
         self.loss = F.mse_loss
+        self.downscale_hires = downscale_hires
+        self.downscale_lowres = downscale_lowres
         self.save_hyperparameters()
 
     def forward(self, x):
         return self.model(x)
 
+    @torch.no_grad()
+    def batch_preprocess(self, batch):
+        y_hires = batch
+        y_hires = y_hires.to(self.device, non_blocking=True)
+
+        y = F.interpolate(
+            y_hires,
+            scale_factor=1 / self.downscale_hires, # for denoising of original photo
+            mode="bicubic",
+            align_corners=False
+        )
+
+        x = F.interpolate(
+            y,
+            scale_factor=1 / self.downscale_lowres,
+            mode="bicubic",
+            align_corners=False
+        )
+
+        return x, y
+
+
     def training_step(self, batch, batch_idx):
-        x, y = batch
+        x, y = self.batch_preprocess(batch)
+
         pred = self(x)
         loss = self.loss(pred, y)
         self.log("train/loss", loss, on_epoch=True, prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        x, y = batch
+        x, y = self.batch_preprocess(batch)
+
         pred = self(x)
         loss = self.loss(pred, y)
         self.log("val/loss", loss, on_epoch=True, prog_bar=True)
@@ -36,28 +69,24 @@ class LitSuperResNet(L.LightningModule):
 
 class ImageLoggerCallback(L.Callback):
     def __init__(self, val_samples, log_every_n_epochs=1):
-        self.val_samples = val_samples
         self.log_every_n_epochs = log_every_n_epochs
-
-        x, y = next(iter(val_samples))
-        self.fixed_x = x
-        self.fixed_y = y
+        self.fixed_batch = next(iter(val_samples))  # HR batch
 
     def on_validation_epoch_end(self, trainer, pl_module):
         epoch = trainer.current_epoch
         if epoch % self.log_every_n_epochs != 0:
             return
 
-        x = self.fixed_x.to(pl_module.device)
-        y = self.fixed_y.to(pl_module.device)
+        pl_module.eval()
 
         with torch.no_grad():
+            x, y = pl_module.batch_preprocess(self.fixed_batch)
             pred = pl_module(x)
 
-        img_gt   = y[:].cpu()
-        img_pred = pred[:].cpu()
+        imgs = torch.cat([y.cpu(), pred.cpu()], dim=2)
+        grid = make_grid(imgs, nrow=4)
 
-        imgs = torch.cat([img_gt, img_pred], dim=2)  # вертикально
-        grid = make_grid(imgs, nrow=4)  # 4 картинки в ряд
+        pl_module.logger.experiment.add_image(
+            "GT_vs_Pred", grid, global_step=epoch
+        )
 
-        pl_module.logger.experiment.add_image("GT_vs_Pred", grid, global_step=epoch)
